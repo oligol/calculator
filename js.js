bfefe52749c3e6463f6f916f9d16b7543af2a4f7
oligol <oligol83@gmail.com>
2022-05-24 21:29:33 +0200

    script version 2

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
 let userInput = document.getElementById("userInput"),
     calculator = document.querySelector('.calculator'),
-    pres = document.querySelectorAll('.pres'),
     expression = '';
 
-// calculator.addEventListener('click', function(e){
-//     if(e.target.classList.contains('pres')) {
-//         expression += e.target.textContent;
-//         userInput.value = expression;
-//     }
-// });
+    calculator.addEventListener('click', function(e){
+        if(e.target.classList.contains('pres')) {
+            expression += e.target.textContent;
+            userInput.value = expression;
+        }else if(e.target.classList.contains('btn-erase')) {
+            expression = "";
+            userInput.value = expression;
+        }else if(e.target.classList.contains('btn-equal')) {
+            userInput.value = eval(expression);
+            expression = "";
+        }
+    });
